f2d88913362b73379ba931b90d26cf5f6fa025b4
Divyansh <a.divyansh.25@gmail.com>
2021-09-16 00:02:18 +0530

    Router added and removed default index page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Header } from './components/Header';
+import { Router } from './components/Router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
